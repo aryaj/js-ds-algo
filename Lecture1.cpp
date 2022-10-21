262d930abfdb5ce91696751c4198e5d6bd7e93df
Arya <ajaarya@apac.corpdir.net>
2022-10-21 21:14:35 +0530

    all commit

---
+++ Lecture1.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+int main() 
+{
+    cout << "I am Ajay Arya" <<endl;
+
+    int a = 124;
+
+    cout<< a <<endl;
+
+    char b = 'v';
+
+    cout<< b <<endl;
+
+    bool b1 = true;
+
+    cout<< b1 <<endl;
+
+    float f = 1.3;
+
+    cout<< f <<endl;
+
+    double d = 1.24;
+
+    cout << d << endl;
+
+    int size = sizeof(a);
+
+    cout << "size of a is " << size <<endl; 
+
+}
